0edd303b7db643238b4594516ae6e87a8889708a
HossamEddine BLAACHA <hossameddine.blaacha@sicpa.com>
2022-05-10 17:02:27 +0100

    install ngx-translate and initialize it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatButtonModule } from "@angular/material/button";
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+import { Constants } from './shared/constants';
+
+export function translateHttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +24,16 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     FontAwesomeModule,
     FlexLayoutModule,
-    MatButtonModule
+    HttpClientModule,
+    MatButtonModule,
+    TranslateModule.forRoot({
+      defaultLanguage: (Constants.LANG_EN),
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+  static readonly LANG_FR = 'fr_FR';
+  static readonly LANG_EN = 'en_GB';
+}
