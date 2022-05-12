872ea10589d5381168cf2956ba5132c85d740c8d
HossamEddine BLAACHA <hossameddine.blaacha@sicpa.com>
2022-05-12 11:57:51 +0100

    feat(): add new library to extact translation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateCompiler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
 
 import { AppComponent } from './app.component';
 import { Constants } from './shared/constants';
@@ -28,6 +29,10 @@ export function translateHttpLoaderFactory(httpClient: HttpClient): TranslateHtt
     MatButtonModule,
     TranslateModule.forRoot({
       defaultLanguage: (Constants.LANG_EN),
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler
+      },
       loader: {
         provide: TranslateLoader,
         useFactory: translateHttpLoaderFactory,
